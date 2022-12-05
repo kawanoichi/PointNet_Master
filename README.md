112e630599a1c3adc655a1d39e919cbdc98db69e
issei <u4xef9rxy21jvrxugfba@docomo.ne.jp>
2022-12-05 18:22:15 +0900

    update: train.pyファイルの更新

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # 研究テーマ「単一画像から３Dモデルを再構築する」  
 宮崎大学大学院　令和4,5年度  
 
-## 環境(before)
-GPU NVIDIA GeForce RTX 3060 Ti<br>
-cuda 11.7<br>
-python 3.9<br>
-numpy 1.19.5<br>
-tensorflow 2.7.0<br>
-
-## 環境(after)
-GPU NVIDIA GeForce RTX 3060 Ti<br>
-cuda 11.7<br>
-python 3.6.13<br>
-numpy 1.19.5<br>
-tensorflow 1.13.1<br>
-pytorch 1.0.1<br>
+## 環境
+GPU        : NVIDIA GeForce RTX 3060 Ti<br>
+cuda       : 11.7<br>
+python     : 3.6.13<br>
+numpy      : 1.19.5<br>
+tensorflow : 1.13.1<br>
+pytorch    : 1.0.1<br>
 
 ## データセット
 ### ShapeNet
@@ -26,5 +19,7 @@ pointcloud<br>
 https://drive.google.com/file/d/1cfoe521iTgcB_7-g_98GYAqO553W8Y0g/view<br>
 
 ### Pix3D
-Pix3D dataset (~20 GB): https://github.com/xingyuansun/pix3d<br>
-Pix3D pointclouds (~13 MB): https://drive.google.com/open?id=1RZakyBu9lPbG85SyconBn4sR8r2faInV<br>
+Pix3D dataset (~20 GB)<br>
+https://github.com/xingyuansun/pix3d<br>
+Pix3D pointclouds (~13 MB)<br>
+https://drive.google.com/open?id=1RZakyBu9lPbG85SyconBn4sR8r2faInV<br>
--- train.py
+++ train.py
@@ -5,31 +5,205 @@
 実行コマンド
 $ bash scripts/train.sh
 """
+import json
 import os
+from datasets import GetShapenetDataset
 import torch
-import argparse
-import random
+import torch.backends.cudnn as cudnn
+import model
+import torch.optim as optim  #最適化アルゴリズムのパッケージ
+from torch.autograd import Variable # 任意のスカラー値関数の自動微分を実装するクラスと関数
+from tqdm import tqdm
 
+
+class train:
+    """機械学習を行うクラス"""
+    def __init__(self, train_param_file):
+        self.__train_param_file = train_param_file
+        self.load_param() #学習パラメータの読み込み
+        self.load_dataset() #データセットの読み込み
+
+    def load_param(self)->None:
+        """インスタンス変数としてパラメータファイルを読み込む.
+        Raises:
+            FileNotFoundError: パラメータファイルが見つからない場合に発生
+            KeyError: パラメータファイルの内容に期待するキーのデータが無い場合に発生
+        """
+        # パラメータファイルが存在するかの確認
+        if not os.path.isfile(self.__train_param_file):
+            raise FileNotFoundError("No file '%s'" % self.__train_param_file)
+
+        with open(self.__train_param_file) as fp:
+            train_param = json.load(fp)
+
+        key = "batchsize"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__batchsize = train_param[key]
+
+        key = "workers"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__workers = train_param[key]
+
+        key = "nepoch"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__nepoch = train_param[key]
+
+        key = "cats"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__cats = train_param[key]
+
+        key = "num_points"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__num_points = train_param[key]
+
+        key = "outfolder"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__outfolder = train_param[key]
+
+        key = "modelG"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__modelG = train_param[key]
+
+        key = "lr"
+        if key not in train_param:
+            raise KeyError("key not found: '%s', file: %s"%(key, self.__train_param_file))
+        self.__lr = train_param[key]
+
+        print("batchsize  :", self.__batchsize)
+        print("workers    :", self.__workers)
+        print("nepoch     :", self.__nepoch)
+        print("cats       :", self.__cats)
+        print("num_points :", self.__num_points)
+        print("outfolder  :", self.__outfolder)
+        print("modelG     :", self.__modelG)
+        print("lr         :", self.__lr)
+
+
+    def load_dataset(self)->None:
+        """インスタンス変数としてデータセットを読み込む.
+        Raises:
+            FileNotFoundError: パラメータファイルが見つからない場合に発生
+        """
+        train_models = 'data/splits/'+'train_models.json'
+        val_models = 'data/splits/'+'val_models.json'
+
+        if not os.path.isfile(train_models):
+            raise FileNotFoundError("No file '%s'" % train_models)
+        
+        with open(train_models, 'r') as f:
+            train_models_dict = json.load(f)
+
+        if not os.path.isfile(val_models):
+            raise FileNotFoundError("No file '%s'" % val_models)
+
+        with open(val_models, 'r') as f:
+            val_models_dict = json.load(f)
+
+        data_dir_imgs = 'data/shapenet/ShapeNetRendering'
+        data_dir_pcl = 'data/shapenet/ShapeNet_pointclouds/'
+
+        # パラメータファイルが存在するかの確認
+        if not os.path.exists(data_dir_imgs):
+            raise FileNotFoundError("No file '%s'" % data_dir_imgs)
+
+        if not os.path.exists(data_dir_pcl):
+            raise FileNotFoundError("No file '%s'" % data_dir_pcl)
+
+        self.__dataset = GetShapenetDataset(data_dir_imgs, data_dir_pcl, train_models_dict, self.__cats, self.__num_points)
+        self.__dataloader = torch.utils.data.DataLoader(self.__dataset, batch_size=self.__batchsize,
+                                                shuffle=True, num_workers=int(self.__workers))
+
+        self.__test_dataset = GetShapenetDataset(data_dir_imgs, data_dir_pcl, val_models_dict, self.__cats, self.__num_points)
+        self.__testdataloader = torch.utils.data.DataLoader(self.__test_dataset, batch_size=self.__batchsize,
+                                                shuffle=True, num_workers=int(self.__workers))
+
+    def train(self):
+        try:
+            os.makedirs(self.__outfolder)
+        except OSError:
+            pass
+
+        # Trueの場合、cuDNNが複数の畳み込みアルゴリズムをベンチマークし、最速のものを選択
+        cudnn.benchmark = True
+
+        # generatorクラスのインスタンス化
+        gen = model.generator(num_points=self.__num_points)
+
+        if not self.__modelG == '':
+            with open(self.__modelG, "rb") as f:
+                gen.load_state_dict(torch.load(f))
+
+        # GPU接続?
+        gen.cuda()
+
+        # 最適化アルゴリズム
+        # optimizerG = optim.RMSprop(gen.parameters(), lr = self.__lr)
+        optimizerG = optim.Adam(gen.parameters(), lr = self.__lr)
+
+        # バッチサイズで分けたグループ数
+        num_batch = len(self.__dataset)/self.__batchsize
+
+        # 学習
+        for epoch in range(self.__nepoch+1):
+            data_iter = iter(self.__dataloader)
+            i = 0
+            # """
+            while i < len(self.__dataloader):
+
+                if i >= len(self.__dataloader):
+                    break
+
+                data = data_iter.next()
+                i += 1
+
+                images, points = data
+
+                points = Variable(points.float())
+                points = points.cuda()
+                images = Variable(images.float())
+                images = images.cuda()
+
+                optimizerG.zero_grad()
+
+                fake, _, _, _ = gen(images)
+                fake = fake.transpose(2, 1)
+
+                lossG1 = batch_NN_loss(points, fake)
+                lossG2 = batch_EMD_loss(points, fake)
+
+                lossG = lossG1 + lossG2
+
+                lossG.backward()
+                optimizerG.step()
+
+                if i % 100 == 0:
+                    print('[%d: %d/%d] train lossG: %f' %(epoch, i, num_batch, lossG.item()))
+
+            if epoch % 20 == 0 and epoch != 0:
+                print("02")
+                self.__lr = self.__lr * 0.5
+                for param_group in optimizerG.param_groups:
+                    param_group['lr'] = self.__lr
+                print('lr decay:', self.__lr)
+
+            if epoch % 50 == 0:
+                print("03")
+                torch.save(gen.state_dict(), '%s/modelG_%d.pth' % (opt.outf, epoch))
+            # """
+            
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)
-    parser.add_argument('--nepoch', type=int, default=50, help='number of epochs to train for')
-    # ALL: ['02691156','02828884','02933112','02958343','03001627','03636649','03211117','04090263','04256520','03691459','04379243','04401088','04530566']
-    parser.add_argument('--cats', default=['02691156'], type=str,
-                        help='Category to train on : ["airplane":02691156, "bench":02828884, "cabinet":02933112, '
-                            '"car":02958343, "chair":03001627, "lamp":03636649, '
-                            '"monitor":03211117, "rifle":04090263, "sofa":04256520, '
-                            '"speaker":03691459, "table":04379243, "telephone":04401088, '
-                            '"vessel"：04530566]')
-    parser.add_argument('--num_points', type=int, default=1024, help='number of epochs to train for, [1024, 2048]')
-    parser.add_argument('--outf', type=str, default='model',  help='output folder')
-    parser.add_argument('--modelG', type=str, default = '', help='generator model path')
-    parser.add_argument('--lr', type=float, default = '0.00005', help='learning rate')
+    train_param_file = "train_param.json"
+    train = train(train_param_file)
+    train.train()
 
-    opt = parser.parse_args()
-    print (opt)
-
+    """
     # 環境変数の宣言
     os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
@@ -40,22 +214,9 @@ if __name__ == "__main__":
     # 無名関数 ※ lamba 引数: 返り値
     blue = lambda x:'\033[94m' + x + '\033[0m'
 
+    # シード値の
     opt.manualSeed = random.randint(1, 10000) # fix seed
     print("Random Seed: ", opt.manualSeed)
-    print(random.seed(opt.manualSeed))
-    # print("Random Seed: ", opt.manualSeed)
-
-
-
-    random.seed(random.randint(1, 10000))
-# 
-    
-
-
-
-
-
-
-
+    # """
 
     print("終了")
